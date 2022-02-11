9b33dfc0aa248dbadf60e7f0e8ce4602643138a3
Jack Pena <penajackc@gmail.com>
2022-02-11 10:19:36 -0700

    commit tree bdbf821e631f2afb7e9c9ab67d202c713f17b5f1

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { directors } from "../data";
+import { directors, movies } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+    const eachDirector = directors.map(director => (
+    <div key={director.name}>
+      <h2>Name: {director.name}</h2>
+      <p>Movies:</p>
+      <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+    </div>))
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {eachDirector}
+  </div>);
 }
 
 export default Directors;
